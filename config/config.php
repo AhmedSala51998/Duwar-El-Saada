88de7da9fab4b7f692317552bf7c5665d0ff8cf4
git <ahmedsalah511998@gmail.com>
2025-09-04 15:21:51 +0300

    config_editing

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ session_start();
 $DB_HOST = getenv('DB_HOST') ?: 'localhost';
 $DB_NAME = getenv('DB_NAME') ?: 'u552468652_duwar_al_saada';
 $DB_USER = getenv('DB_USER') ?: 'u552468652_duwar_al_saada';
-$DB_PASS = getenv('DB_PASS') ?: 'Duwar_al_saada';
+$DB_PASS = getenv('DB_PASS') ?: 'Duwar_al_saada12345@';
 
 try{
   $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",$DB_USER,$DB_PASS,[
