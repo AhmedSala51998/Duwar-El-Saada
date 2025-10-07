29e323c9bd5c679da39a24ce0ab93aeec874a12a
git <ahmedsalah511998@gmail.com>
2025-10-07 17:13:30 +0300

    fgfhghgfhgf

--- assetes.php
+++ assetes.php
@@ -154,7 +154,7 @@ $can_edit = in_array(current_role(), ['admin','manager']);
               <input name="type" class="form-control" value="<?= esc($r['type']) ?>">
 
               <label>العدد</label>
-              <input type="number" name="quantity" class="form-control" value="<?= (int)$r['quantity'] ?>" min="1">
+              <input type="number" name="quantity" id="quantity_edit_<?= $r['id'] ?>" class="form-control" value="<?= (int)$r['quantity'] ?>" min="1">
 
               <label>السعر</label>
               <input type="number" step="0.01" name="price" id="price_edit_<?= $r['id'] ?>" class="form-control" 
