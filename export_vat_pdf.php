ceff65b1be40bd61cf8d753a4125edb25b73419e
git <ahmedsalah511998@gmail.com>
2025-10-07 17:53:24 +0300

    لاالبالبالبفغ

--- export_vat_pdf.php
+++ export_vat_pdf.php
@@ -1,5 +1,9 @@
 <?php
 require __DIR__.'/config/config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_auth();
 
 $from_date = $_GET['from_date'] ?? '';
