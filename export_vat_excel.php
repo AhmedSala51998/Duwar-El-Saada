1f8bc7e99b52d2eb4981b99e06ed167a330c1423
git <ahmedsalah511998@gmail.com>
2025-10-07 18:02:46 +0300

    ghjghjghj

--- export_vat_excel.php
+++ export_vat_excel.php
@@ -1,5 +1,8 @@
 <?php
 require __DIR__.'/config/config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_auth();
 
 require_once __DIR__.'/libs/SimpleXLSXGen.php';
