57b3fa986e80bcb8061ce929bbd7122657f8690a
git <ahmedsalah511998@gmail.com>
2025-11-04 11:07:43 +0300

    jkjkjhk

--- expenses.php
+++ expenses.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require __DIR__.'/config/config.php';
+require __DIR__.'/partials/header.php';
 require_role(['admin','manager']);
 
 // جلب العهد
