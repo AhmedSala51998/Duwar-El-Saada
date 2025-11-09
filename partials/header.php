c68ee65beb63ffa344e70128e9d10d37136b0e06
git <ahmedsalah511998@gmail.com>
2025-11-09 18:46:43 +0300

    kjkjhkhjkhj

--- partials/header.php
+++ partials/header.php
@@ -614,10 +614,10 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <a href="<?= BASE_URL ?>/home.php" 
       class="navbar-brand d-flex align-items-center text-decoration-none">
       <img src="<?= BASE_URL ?>/assets/logo_header2.png" 
-          width="width:300px" height="150px" 
+          width="auto" height="150px" 
           style="transform: scale(3.5);"
           alt="logo" 
-          class="">
+          class="rounded">
     </a>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
   </div>
