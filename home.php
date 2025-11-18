c52c3c3f721b104bd027b59c1cf8a8c85e539eb7
git <ahmedsalah511998@gmail.com>
2025-11-18 13:29:50 +0300

    ghjghjghj

--- home.php
+++ home.php
@@ -28,7 +28,6 @@ body {
   height: 220px;
   background: rgba(255,255,255,0.35);
   border-radius: 50%;
-  top: -60px;
   right: -60px;
   filter: blur(25px);
 }
@@ -50,7 +49,7 @@ body {
 /* ====== اللوجو الطائر (Animated Floating Logo) ====== */
 .floating-logo {
   position: absolute;
-  top: 4px;   /* 👈 بدّلها كده */
+  top: -50px;   /* 👈 بدّلها كده */
   left: 20px;
   width: 90px;
   height: 90px;
