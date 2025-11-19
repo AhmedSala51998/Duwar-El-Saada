a3e76c21463d18c015138f9f0ba0b3451a29615e
git <ahmedsalah511998@gmail.com>
2025-11-19 17:28:17 +0300

    trtyuutyut

--- partials/header.php
+++ partials/header.php
@@ -2236,7 +2236,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
     /* الخلفية الداكنة لحقل الرقم */
     body.dark-mode input[type="number"] {
-      background: #1e1e1e;
+      background: #1f1f1f;
       color: #fff;
       border-color: #444;
     }
@@ -2244,7 +2244,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     /* تأثير على الأسهم */
     body.dark-mode input[type="number"]::-webkit-inner-spin-button,
     body.dark-mode input[type="number"]::-webkit-outer-spin-button {
-      background: #333;      /* لون خلفية الأسهم */
+      background: #1f1f1f;      /* لون خلفية الأسهم */
       border-radius: 4px;
       filter: invert(1);     /* يخلي لون الأسهم أبيض */
     }
