1bcc18bc1610a287c98e7a221174c37d394248a1
git <ahmedsalah511998@gmail.com>
2025-11-19 12:56:54 +0300

    ghkjhkhjkhj

--- partials/header.php
+++ partials/header.php
@@ -1963,7 +1963,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
     /* تحديث شكل select2 ليكون مثل باقي الحقول */
     .select2-container .select2-selection--single {
-        height: 20px !important;
+        height: 35px !important;
         padding: -5px 10px !important;
         border: 1px solid #d1d5db !important;
         border-radius: 10px !important;
