b2343cf72854de1efc3a077253f5be67e51c18b6
git <ahmedsalah511998@gmail.com>
2025-11-24 12:26:39 +0300

    hjgjjgh

--- partials/header.php
+++ partials/header.php
@@ -2621,6 +2621,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
   body.dark-mode .sidebar-link.active {
     background: var(--primary);
     color: #FFF !important;
+    box-shadow:none;
+    border:none
   }
 
   body.dark-mode .sidebar-link.active i {
